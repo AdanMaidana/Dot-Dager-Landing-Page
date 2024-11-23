134e1d0cf7cf9adb2719a3d9e58230e5c6e8e00a
Adan Maidana <adanmaidana31@gmail.com>
2024-11-23 02:17:55 -0300

    bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adanmaidana.github.io/Dot-Dager-Landing-Page/',
+  buildOptions: {
+    site: 'https://adanmaidana.github.io/Dot-Dager-Landing-Page/', // Asegúrate de poner la URL correcta
+    base: '/Dot-Dager-Landing-Page/',  // Añades la ruta del subdirectorio de GitHub Pages
+  },
   integrations: [tailwind()]
 });
